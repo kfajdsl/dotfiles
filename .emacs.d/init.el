52f6c75319370ff2288bcdd451361b6e6305b426
Sahan Reddy <sahan.reddy.58@gmail.com>
2021-04-03 13:41:32 -0400

    lots of stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,43 +12,132 @@
   (load bootstrap-file nil 'nomessage))
 
  
-;; Packages
+;;; Packages
  
 (straight-use-package 'use-package)
+(setq straight-use-package-by-default t) ; always use :straight t
+(setq use-package-always-demand (daemonp)) ; if emacs is running in a daemon, load all packages immediately
 
 (use-package evil
-  :straight t
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
+  (setq evil-split-window-below t)
+  (setq evil-vsplit-window-right t)
   :config
   (evil-mode 1))
 
 (use-package evil-collection
   :after evil
-  :straight t
   :config
   (evil-collection-init))
 
+;; We don't call (projectile-mode 1) because counsel-projectile takes care of that
+(use-package projectile
+  :config
+  (setq projectile-project-search-path '("~/code/")))
+
 (use-package ivy
-  :straight t
   :config
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t))
 
 (use-package counsel
-  :straight t)
+  :config
+  (counsel-mode 1))
+
+(use-package counsel-projectile
+  :config
+  (counsel-projectile-mode 1))
 
 (use-package company
-  :straight t)
+  :config
+  (company-mode 1)
+  (setq company-dabbrev-downcase 0)
+  (setq company-idle-delay 0))
 
-;; Config
+(use-package typescript-mode
+	:config
+	(setq-default typescript-indent-level 2))
 
+(setq-default js-indent-level 2)
+
+(use-package lsp-mode
+  :hook ((js-mode	  ; ts-ls (theia-ide)
+	  js-jsx-mode 	  ; ts-ls (theia-ide)
+	  typescript-mode ; ts-ls (theia-ide)
+	  ) . lsp)
+  :config
+  (setq lsp-keymap-prefix "SPC l"))
+
+(use-package lsp-ui
+  :config
+  (setq lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-doc-show-with-cursor nil))
+(use-package lsp-ivy)
+
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-vibrant t))
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package general)
+
+;;; Keybindings
+
+;; One less keypress than :
+(general-swap-key nil 'motion
+  ";" ":")
+
+(general-define-key
+ ;; Window Motion
+ "M-h" 'evil-window-left
+ "M-j" 'evil-window-down
+ "M-k" 'evil-window-up
+ "M-l" 'evil-window-right
+
+ "C-x C-e" 'eval-region)
+
+(general-create-definer leader-def
+  :states '(normal visual) 
+  :keymaps 'override
+  :prefix "SPC"
+  :non-normal-prefix "S-SPC")
+
+(leader-def
+  "." 'find-file
+  "b" 'buffer-menu
+  "p" 'projectile-command-map)
+
+
+;;; Config
+
+;; (set-frame-font ...) in init.el doesn't work for emacsclient
+;;   since it needs to be run for every frame
+(add-to-list 'default-frame-alist '(font . "Iosevka SS02-12"))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(setq mouse-autoselect-window t)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
 (setq-default display-line-numbers 'relative)
 
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory ".emacs-backups"))))
 
 
-;; Customize
+
+;;; Customize
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
